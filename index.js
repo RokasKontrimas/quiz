8829ded00e4ca05aa2fca418843466d5dc3f559d
RokasKontrimas <rokas.kontrimas123@gmail.com>
2024-02-28 15:34:19 +0200

    fixed logic

--- index.js
+++ index.js
@@ -1,93 +1,121 @@
+// quiz one
 var quizAnswers1 = 0;
+
+let question11Correct = false;
+let question12Correct = false;
+
+let question11 = "Kokios spalvos yra morka?";
+let question12 = "Ar apelsinas yra oranzinis?";
+
+let question11Answer = "oranzine";
+let question12Answer = "taip";
+
+let userAnswer11 = "oranzine";
+let userAnswer12 = "taip";
+
+// quiz two
 var quizAnswers2 = 0;
+
+let question21Correct = false;
+let question22Correct = false;
+
+let question21 = "Ar duona yra skanesne uz batona?";
+let question22 = "Ar bandele skaitosi kaip duonos kepinys?";
+
+let question21Answer = "ne";
+let question22Answer = "taip";
+
+let userAnswer21 = "ne";
+let userAnswer22 = "taip";
+
+// quiz three
 var quizAnswers3 = 0;
 
-let question1Correct = false;
-let question2Correct = false;
-let question3Correct = false;
+let question31Correct = false;
+let question32Correct = false;
+let question33Correct = false;
 
-let level1 = 1;
-let level2 = 2;
-let level3 = 3;
+let question31 = "Ar jautienos kepsnys yra skanesnis uz kiaulienos kepsny?";
+let question32 = "Ar kiaulienos kepsnys yra skanesnis uz vistienos kepsny?";
+let question33 = "ar vistienos kepsnys yra skanesnis uz jautienos kepsny?";
 
-let question1 = "Kokios spalvos yra morka?";
-let question2 = "Ar apelsinas yra oranzinis?";
-let question3 = "Ar citrina yra rugstesne uz laima?";
+let question31Answer = "ne";
+let question32Answer = "ne";
+let question33Answer = "ne";
 
-let question1Answer = "oranzine";
-let question2Answer = "taip";
-let question3Answer = "nezinau";
+let userAnswer31 = "taip";
+let userAnswer32 = "asdas";
+let userAnswer33 = "ne";
 
-let userAnswer1 = "oranzine";
-let userAnswer2 = "taip";
-let userAnswer3 = "nezinau";
+
 // Pirmas lygis
-if (level1) {
-    if (question1 && question1Answer === userAnswer1) {
-        console.log(++quizAnswers1);
-        question1Correct = true;
-    }
-    if (question2 && question2Answer === userAnswer2) {
-        console.log(++quizAnswers1);
-        question2Correct = true;
+if (question11Answer === userAnswer11) {
+    console.log(++quizAnswers1);
+    question21Correct = true;
+}
+if (question12Answer === userAnswer12) {
+    console.log(++quizAnswers1);
+    question22Correct = true;
 
-    }
-    if (quizAnswers1 > 1) {
-        console.log('1 Lygis: i abu klausimus atsakei teisingai ir patekai i kita lygi!');
-    } else if (quizAnswers1 > 0 && question1Correct === false) {
-        console.log(`1Lygis: Patekai i kita lygi, bet blogai atsakei i \'${question1}\' klausima`)
-    } else if (quizAnswers1 > 0 && question2Correct === false) {
-        console.log(`1Lygis: Patekai i kita lygi, bet blogai atsakei i \'${question2}\' klausima`)
-    } else if (quizAnswers1 === 0) {
-        console.log('1 Lygis: abu atsakymus pateikei klaidingus ir nepatekai i kita lygi..');
-    }
+}
+if (quizAnswers1 > 1) {
+    console.log('1 Lygis: i abu klausimus atsakei teisingai ir patekai i kita lygi!');
+} else if (quizAnswers1 > 0 && question11Correct === false) {
+    console.log(`1Lygis: Patekai i kita lygi, bet blogai atsakei i \'${question11}\' klausima`)
+} else if (quizAnswers1 > 0 && question12Correct === false) {
+    console.log(`1Lygis: Patekai i kita lygi, bet blogai atsakei i \'${question12}\' klausima`)
+} else if (quizAnswers1 === 0) {
+    console.log('1 Lygis: abu atsakymus pateikei klaidingus ir nepatekai i kita lygi..');
 }
 // Antras lygis
-if (level2) {
-    if (question1 && question1Answer === userAnswer1) {
-        console.log(++quizAnswers2);
-        question1Correct = true;
-    }
-    if (question2 && question2Answer === userAnswer2) {
-        console.log(++quizAnswers2);
-        question2Correct = true;
-
-    }
-
-    if (quizAnswers2 > 1) {
-        console.log(`2 Lygis: atsakei i ${quizAnswers2}/2 klausimus ir patekai i kita lygi!`);
-    } else if (quizAnswers2 > 0 && question1Correct === false) {
-        console.log(`2Lygis: nepatekai i kita lygi nes blogai atsakei i \'${question1}\' klausima`)
-    } else if (quizAnswers2 > 0 && question2Correct === false) {
-        console.log(`2Lygis: nepatekai i kita lygi nes blogai atsakei i \'${question2}\' klausima`)
-    } else if (quizAnswers2 === 0) {
-        console.log('2 Lygis: abu atsakymus pateikei klaidingus ir nepatekai i kita lygi..');
-    }
+if (question21Answer === userAnswer21) {
+    console.log(++quizAnswers2);
+    question1Correct = true;
 }
-// Trecias lygis
-if (level3) {
-    if (question1 && question1Answer === userAnswer1) {
-        console.log(++quizAnswers3);
-        question1Correct = true;
-    }
-    if (question2 && question2Answer === userAnswer2) {
-        console.log(++quizAnswers3);
-        question2Correct = true;
+if (question22Answer === userAnswer22) {
+    console.log(++quizAnswers2);
+    question2Correct = true;
 
-    }
-    if (question3 && question3Answer === userAnswer3) {
-        console.log(++quizAnswers3);
-        question3Correct = true;
-    }
-    if (quizAnswers3 > 2) {
-        console.log(`3 Lygis: atsakei i ${quizAnswers3}/3 klausimus teisingai ir patekai i kita lygi!`);
-    } else if (quizAnswers3 > 0 && question1Correct === false) {
-        console.log(`3 Lygis: nepatekai i kita lygi nes blogai atsakei i \'${question1}\' klausima`)
-    } else if (quizAnswers3 > 0 && question2Correct === false) {
-        console.log(`3 Lygis: nepatekai i kita lygi nes blogai atsakei i \'${question2}\' klausima`)
-    } else if (quizAnswers3 > 0 && question3Correct === false) {
-        console.log(`3 Lygis: nepatekai i kita lygi nes blogai atsakei i \'${question3}\' klausima`)
-    } else if (quizAnswers3 > 1) {
-        console.log('3 Lygis: abu atsakymus pateikei klaidingus ir nepatekai i kita lygi..');
-    }
+}
+
+if (quizAnswers2 > 1) {
+    console.log(`2 Lygis: atsakei i ${quizAnswers2}/2 klausimus ir patekai i kita lygi!`);
+} else if (quizAnswers2 > 0 && question21Correct === false) {
+    console.log(`2Lygis: nepatekai i kita lygi nes blogai atsakei i \'${question21}\' klausima`)
+} else if (quizAnswers2 > 0 && question22Correct === false) {
+    console.log(`2Lygis: nepatekai i kita lygi nes blogai atsakei i \'${question22}\' klausima`)
+} else if (quizAnswers2 === 0) {
+    console.log('2 Lygis: abu atsakymus pateikei klaidingus ir nepatekai i kita lygi..');
+}
+
+// trecias lygis
+if (question31Answer === userAnswer31) {
+    console.log(++quizAnswers3);
+    question31Correct = true;
+}
+if (question32Answer === userAnswer32) {
+    console.log(++quizAnswers3);
+    question32Correct = true;
+
+}
+if (question33Answer === userAnswer33) {
+    console.log(++quizAnswers3);
+    question33Correct = true;
+}
+if (quizAnswers3 > 2) {
+    console.log(`3 Lygis: atsakei i ${quizAnswers3}/3 klausimus teisingai ir patekai i kita lygi!`);
+} else if (quizAnswers3 > 1 && question31Correct && question32Correct && !question33Correct) {
+    console.log(`3 Lygis: Patekai i kita lygi, bet blogai atsakei i \'${question31} \' klausima`)
+} else if (quizAnswers3 > 1 && question31Correct && !question32Correct && question33Correct) {
+    console.log(`3 Lygis: Patekai i kita lygi, bet blogai atsakei i \'${question32} \' klausima`)
+} else if (quizAnswers3 > 1 && !question31Correct && question32Correct && question33Correct) {
+    console.log(`3 Lygis: Patekai i kita lygi, bet blogai atsakei i \'${question31} \' klausima`)
+} else if (quizAnswers3 > 0 && question31Correct && !question32Correct && !question33Correct) {
+    console.log(`3 Lygis: nepatekai i kita lygi nes blogai atsakei i \'${question32}\' ir \'${question33} \' klausimus`)
+} else if (quizAnswers3 > 0 && !question31Correct && question32Correct && !question33Correct) {
+    console.log(`3 Lygis: nepatekai i kita lygi nes blogai atsakei i \'${question32}\' ir \'${question33} \' klausimus`)
+} else if (quizAnswers3 > 0 && !question31Correct && !question32Correct && question33Correct) {
+    console.log(`3 Lygis: nepatekai i kita lygi nes blogai atsakei i \'${question32}\' ir \'${question33} \' klausimus`)
+} else if (quizAnswers3 < 1) {
+    console.log(`3 Lygis: ${quizAnswers3} atsakymus pateikei klaidingus ir nepatekai i kita lygi..`);
 }
